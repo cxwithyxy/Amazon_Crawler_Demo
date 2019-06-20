7c9f69e4496c7d332ea39e588d405196a5a865fd
chenxuan <523764720@qq.com>
2019-06-20 14:14:51 +0800

    现在能拿到商品第一个价格了

--- Big_boss.ts
+++ Big_boss.ts
@@ -14,7 +14,8 @@ export class Big_boss extends Manager
     {
         super()
         this.inject_js = new Inject_js_handler([
-            `${__dirname}/inject_js_lib/main_page_do`
+            `${__dirname}/inject_js_lib/main_page_do`,
+            `${__dirname}/inject_js_lib/search_result_do`,
         ])
     }
 
@@ -22,7 +23,9 @@ export class Big_boss extends Manager
     {
         this.init_worker()
         await this.goto_main_page()
-        await this.search_something(`雨伞`)
+        let result_links = await this.search_something(`雨伞`)
+        console.log(`current page result count: ${result_links.length}`)
+        await this.print_all_item_info(result_links)
     }
 
     init_worker()
@@ -36,6 +39,11 @@ export class Big_boss extends Manager
         this.get_main_worker().page_init()
     }
 
+    /**
+     * 打开首页
+     *
+     * @memberof Big_boss
+     */
     async goto_main_page()
     {
         await this.workers_do(async (_worker) => {
@@ -44,12 +52,36 @@ export class Big_boss extends Manager
         })
     }
 
-    async search_something(_search_key: string)
+    /**
+     * 搜索内容
+     *
+     * @param {string} _search_key 关键词
+     * @returns {Promise<Array<string>>} 返回当前搜索结果商品链接
+     * @memberof Big_boss
+     */
+    async search_something(_search_key: string): Promise<Array<string>>
     {
+        let re_array: Array<string> = [];
         await this.workers_do(async (_worker) => {
             await _worker.exec_js(`input_search_bar("${_search_key}")`)
             await _worker.click(585, 69)
             await _worker.wait_page_load()
+            re_array = await _worker.exec_js(`get_item_links()`)
         })
+        return re_array;
+    }
+
+    async print_all_item_info(_item_links: Array<string>)
+    {
+        let _item_links_clone = _item_links.concat([]);
+        await this.workers_do(async (_worker) =>
+        {
+            let price_string = "no price";
+            _worker.open_url(<string>_item_links_clone.shift())
+            await _worker.wait_page_load()
+            price_string = await _worker.exec_js(`get_item_price_string()`)
+            console.log(price_string);
+        })
+        
     }
 }
---
+++ inject_js_lib/search_result_do.ts
@@ -0,0 +1,29 @@
+let get_page_items: Function
+
+export = {
+    get_page_items: () =>
+    {
+        return document.querySelectorAll("#search > div.sg-row > div.sg-col-20-of-24.sg-col-28-of-32.sg-col-16-of-20.sg-col.s-right-column.sg-col-32-of-36.sg-col-8-of-12.sg-col-12-of-16.sg-col-24-of-28 > div > span:nth-child(4) > div.s-result-list.s-search-results.sg-row > div")
+    },
+
+    get_item_links: () =>
+    {
+        let return_array:Array<string> = [];
+        let item_doms = get_page_items();
+        item_doms.forEach((item: HTMLDivElement) =>
+        {
+            try {
+                let temp_href = item.querySelectorAll("a")[0].href;
+                return_array.push(temp_href)
+            } catch (error) {
+                console.log(error);
+            }
+        });
+        return return_array
+    },
+
+    get_item_price_string: () =>
+    {
+        return (<string>document.querySelectorAll("#priceblock_saleprice")[0].textContent).substring(1)
+    }
+}
